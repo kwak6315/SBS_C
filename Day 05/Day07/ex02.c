20719ea0e18b792ec57ebee61c620bca2b843caa
kwak6315 <kwak6315@naver.com>
2022-02-14 15:49:04 +0900

    2/14(월)

---
+++ Day 05/Day07/ex02.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(void) {
+    int num;
+
+
+
+    //무한반복 : 조건을 참(1)로 주면 무한히 반복된다. 
+    while(1) {
+        printf("1.주문하기,  2. 장바구니, 0. 종료\n");
+        printf("메뉴번호 : ");
+        scanf("%d", &num);
+        
+        
+        if(num ==1 )
+            printf("주문되었습니다.\n");
+        if(num == 2)
+            printf("장바구니에 담겼습니다.\n");
+
+        //종료조건
+        if(num==0) {
+            printf("종료합니다.\n");
+            break;
+            //break : 현재속한 조건 문, 반복문을 벗어나는명령
+        }
+            }
+    
+    return 0;
+}
